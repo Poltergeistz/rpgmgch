19d6505da7f1399471ef7f14f595238135655955
david <david.journo.31@gmail.com>
2018-08-22 15:33:20 +0200

    Cloth class extends Product

---
+++ Cloth.php
@@ -0,0 +1,25 @@
+<?php
+class Cloth extends Product
+{
+    private $_brand;
+
+    public function __construct($id, $name, $price,$brand)
+    {
+        parent::__construct($id, $name, $price);
+        $this->setBrand($brand);
+    }
+
+    public function try()
+    {
+
+    }
+    public function setBrand($data)
+    {
+        $this->_brand = $data;
+    }
+    public function getBrand()
+    {
+        return $this->_brand;
+    }
+
+}
