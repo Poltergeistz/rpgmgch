ff02648ab036600ec12e84cdae6cde10dbd6a95d
david <david.journo.31@gmail.com>
2018-08-26 17:08:51 +0200

    Step 4 : Models folder everything is working

---
+++ models/Cloth.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__.'/Product.php';
+class Cloth extends Product
+{
+    protected $_brand;
+
+    public function __construct($id, $name, $price,$brand)
+    {
+        parent::__construct($id, $name, $price);
+        $this->setBrand($brand);
+    }
+
+    public function try()
+    {
+
+    }
+    public function setBrand($data)
+    {
+        $this->_brand = $data;
+    }
+    public function getBrand()
+    {
+        return $this->_brand;
+    }
+
+}
---
+++ models/User.php
@@ -0,0 +1,44 @@
+<?php 
+class User{
+    protected $_id;
+    protected $_email;
+    protected $_createdAt;
+
+    public function __construct($id, $email, $createdAt)
+    {
+    $this->setId($id);
+    $this->setEmail($email);
+    $this->setCreatedAt($createdAt);
+    }
+
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    public function setId($data)
+    {
+        $this->_id = $data;
+    }
+
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    public function setEmail($data)
+    {
+        $this->_email = $data;
+    }
+
+    public function getCreatedAt()
+    {
+    return $this->_createdAt;
+    }
+
+    public function setCreatedAt($data)
+    {
+        $this->_createdAt = $data;
+    }
+
+}
