938f4ad4e24adc7378c83e7c570b1bcdbba98dbd
david <david.journo.31@gmail.com>
2018-08-26 19:00:30 +0200

    Step 5 : Refactorization - Solved ID bug

--- data/products.php
+++ data/products.php
@@ -6,10 +6,11 @@ require_once __DIR__.'/../models/Vegetable.php';
 $vetement1 = new Cloth('1','Tie','40','TotoInc');
 $vetement2 = new Cloth('2','Shirt','50','TotoInc');
 $vetement3 = new Cloth('3','Pant','70','TotoInc');
-
-$vegetable1 = new Vegetable('1','Beans','3','BeansInc','20/08/18');
-$vegetable2 = new Vegetable('2','Potato','2','PotatoInc','22/08/18');
-
+// Bug resolu problème d'ID voir commentaire validateOrder 
+$vegetable1 = new Vegetable('4','Beans','3','BeansInc','20/08/18');
+                    // was  '1'
+$vegetable2 = new Vegetable('5','Potato','2','PotatoInc','22/08/18');
+                    // was  '2'
 return [$vetement1,$vetement2,$vetement3,$vegetable1,$vegetable2];
 ?>
 
