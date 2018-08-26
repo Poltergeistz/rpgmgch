61e4be90c6e2d9fed4261880637cd72c44d93490
david <david.journo.31@gmail.com>
2018-08-26 19:01:20 +0200

    Step 5 : ValidateOrder page

--- views/validateOrder.php
+++ views/validateOrder.php
@@ -12,32 +12,53 @@ $products = require_once __DIR__."/../data/products.php";
 </head>
 <body>
 <?php
+
+/*
+
+Expects the same products as input/outputs but fails
+Does not put some user products from shopping in validateOrder
+
+Si je commande que des vetements en tant que boby ou johns, j'ai dans mon pannier un bean, une potato, et un pant
+ce qui n'est pas attendu.. le problème est aussi présent dans plusieurs cas
+
+####### Resolu ####
+Après avoir cherché je me suis rendu compte que l'ID de certains vétements étaient la même que les légumes.
+J'ai donc modifié l'ID et tout fonctionne comme attendu.
+
+J'ai aussi laissé en commentaire ce que j'avais fait pour que ça soit plus lisible.
+*/
+
+// Cannot be NULL
 if(isset($_POST['submit'])&&isset($_POST['client'])&&isset($_POST['product0'])&&isset($_POST['product1'])&&isset($_POST['product2']))
 {
-	foreach($users as $val){
-		if ($val->getId()==$_POST['client'])
-            $client=$val;
+    foreach($users as $val1)
+    {
+		if ($val1->getId()==$_POST['client'])
+            $client=$val1;
+    }
+    foreach($products as $val2)
+    {
+		if ($val2->getId()==$_POST['product0'])
+			$product1=$val2;
 	}
-	foreach($products as $val){
-		if ($val->getId()==$_POST['product0'])
-			$product1=$val;
+    foreach($products as $val3)
+    {
+		if ($val3->getId()==$_POST['product1'])
+			$product2=$val3;
 	}
-	foreach($products as $val){
-		if ($val->getId()==$_POST['product1'])
-			$product2=$val;
-	}
-	foreach($products as $val){
-		if ($val->getId()==$_POST['product2'])
-			$product3=$val;
+    foreach($products as $val4)
+    {
+		if ($val4->getId()==$_POST['product2'])
+			$product3=$val4;
     }
 	$client->Buy($product1);
 	$client->Buy($product2);
 	$client->Buy($product3);
     $cart = $client->getCart();
     echo '<h2>Panier du client : ' . $client->getId() . '</h2>';
-	foreach($cart as $val)
+	foreach($cart as $value)
 	{
-		echo '<p>'.$val->getName().' : '.$val->getPrice().'€</p>';
+		echo '<p>'.$value->getName().' : '.$value->getPrice().'€</p>';
 		echo '<hr>';
 	}
 	echo '<h3>Total : '.$client->getBillAmount().'€</h3>';
